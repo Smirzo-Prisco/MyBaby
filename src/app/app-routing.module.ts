697f22081fae05abb4d113ac4f60817eebfc8997
Roberto Bottini <roberto.bottini@easyweb.it>
2020-01-24 16:15:15 +0100

    CHERRY PICK 2564934 - EASYP13 dashboard security punto 2.1 della specifica FIX

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'login',
+  //   pathMatch: 'full'
+  // },
   {
     path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'home',
---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'list',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../pages/list/list.module').then(m => m.ListPageModule)
+          }
+        ]
+      },
+      {
+        path: 'home',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../pages/home/home.module').then(m => m.HomePageModule)
+          }
+        ]
+      },
+      {
+        path: 'timeline',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../pages/timeline/timeline.module').then( m => m.TimelinePageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/list',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/list',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
