cd68820fd2004a1dbe8e5cdb6dd6ffb26a2778da
Roberto Bottini <roberto.bottini@easyweb.it>
2020-01-30 10:01:51 +0100

    vano tentativo di aggiungere un tab menu

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,58 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+
 import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'tabs/home', pathMatch: 'full' },
   {
     path: 'tabs',
     component: TabsPage,
     children: [
-      {
-        path: 'list',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../pages/list/list.module').then(m => m.ListPageModule)
-          }
-        ]
-      },
-      {
-        path: 'home',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../pages/home/home.module').then(m => m.HomePageModule)
-          }
-        ]
-      },
-      {
-        path: 'timeline',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../pages/timeline/timeline.module').then( m => m.TimelinePageModule)
-          }
-        ]
-      },
-      {
-        path: '',
-        redirectTo: '/tabs/list',
-        pathMatch: 'full'
-      }
+      { path: 'home', loadChildren: () => import('../pages/home/home.module').then(m => m.HomePageModule) },
+      { path: 'list', loadChildren: () => import('../pages/list/list.module').then(m => m.ListPageModule) }
     ]
-  },
-  {
-    path: '',
-    redirectTo: '/tabs/list',
-    pathMatch: 'full'
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class TabsPageRoutingModule {}
