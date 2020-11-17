4ac9fd23a3d83c9477cd6908d512fb78cb261d20
Roberto Bottini <roberto.bottini@easyweb.it>
2020-11-17 23:45:58 +0100

    costruzione icona like delle foto

--- src/app/pages/open-multimedia/open-multimedia.page.ts
+++ src/app/pages/open-multimedia/open-multimedia.page.ts
@@ -7,19 +7,20 @@ import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player/ngx';
 import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-open-multimedia',
-  templateUrl: './open-multimedia.page.html',
-  styleUrls: ['./open-multimedia.page.scss'],
+selector: 'app-open-multimedia',
+templateUrl: './open-multimedia.page.html',
+styleUrls: ['./open-multimedia.page.scss'],
 })
 
 export class OpenMultimediaPage implements OnInit {
-  id = null;
-  type = '';
-  public item: any;
-  // public item: Array<any>;
-  public env: any;
+id = null;
+type = '';
+public item: any;
+public env: any;
+like = false;
+likeToggleIcon = 'heart';
 
-  constructor(
+constructor(
     private activatedRoute: ActivatedRoute,
     private dataService: DataService,
     private commonService: CommonService,
@@ -32,11 +33,25 @@ export class OpenMultimediaPage implements OnInit {
 
     this.item = this.activatedRoute.snapshot.data['multimedia'];
     this.env = environment;
+    this.like = this.item.likeDetails.me;
+    this.toggleLikeIcon();
   }
 
   sanitizeUrl(yt_id){
       let ytParams = '?showinfo=0&frameborder=0&iv_load_policy=3&modestbranding=1&rel=0&autoplay=1&playsinline=0';
 
       return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + yt_id + ytParams);
+  }
+
+  setLike(id, type) {
+    this.dataService.setLike(id, type, this.like);
+  }
+
+  toggleLikeIcon(): void {
+    if(!this.like) {
+      this.likeToggleIcon = 'heart-outline';
+    } else {
+      this.likeToggleIcon = 'heart';
+    }
   }
 }
